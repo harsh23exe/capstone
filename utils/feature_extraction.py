7a3691d36da883b35133908ddec3ea766451e79a
bhawanaprasain <bhawana.prs@gmail.com>
2026-02-13 13:51:40 -0500

    Extract features for train dataset

--- utils/feature_extraction.py
+++ utils/feature_extraction.py
@@ -2,7 +2,8 @@ import torch
 from transformers import AutoModelForCausalLM
 from tokenizers import Tokenizer
 from utils.config import model_name
-
+from settings import train_df_path, test_df_path
+from utils.file_handler import read_csv
 class ProGenEmbeddingExtractor:
     def __init__(self, model_name="hugohrban/progen2-small", device=None):
         self.device = device if device else ("cuda" if torch.cuda.is_available() else "cpu")
@@ -17,38 +18,35 @@ class ProGenEmbeddingExtractor:
         
         self.model.eval()
 
-    def get_embedding(self, sequence, pooling="last"):
-        """
-        sequence: str
-        pooling: "last" or "mean"
-        returns: torch tensor (hidden_size,)
-        """
-        
-        input_ids = torch.tensor(
-            [self.tokenizer.encode(sequence).ids]
-        ).to(self.device)
+    def get_embedding(self, sequence, pooling="tokens"):
+        input_ids = torch.tensor([self.tokenizer.encode(sequence).ids]).to(self.device)
 
         with torch.no_grad():
-            outputs = self.model(
-                input_ids,
-                output_hidden_states=True
-            )
+            outputs = self.model(input_ids, output_hidden_states=True)
 
-        last_hidden_state = outputs.hidden_states[-1]  
+        last_hidden_state = outputs.hidden_states[-1]  # (1, L, 1024)
 
-        if pooling == "last":
-            embedding = last_hidden_state[:, -1, :] 
+        if pooling == "tokens":
+            embedding = last_hidden_state.squeeze(0)          # (L, 1024)
+        elif pooling == "last":
+            embedding = last_hidden_state[:, -1, :].squeeze(0) # (1024,)
         elif pooling == "mean":
-            embedding = last_hidden_state.mean(dim=1)  
+            embedding = last_hidden_state.mean(dim=1).squeeze(0) # (1024,)
         else:
-            raise ValueError("pooling must be 'last' or 'mean'")
+            raise ValueError("pooling must be 'tokens', 'last', or 'mean'")
 
-        return embedding.squeeze(0)  
+        return embedding
 
+
+
+train_df = read_csv(train_df_path)
 extractor = ProGenEmbeddingExtractor(model_name=model_name)
+print(train_df.columns)
 
-seq = "1MEVVIVTGMSGAGK"
+for index, row in train_df[:10].iterrows():
+    seq = row['original_sequence']
+    print(len(seq))
+    embedding = extractor.get_embedding(seq, pooling="tokens")
+    print("Embedding shape:", embedding.shape)
 
-embedding = extractor.get_embedding(seq, pooling="last")
 
-print("Embedding shape:", embedding.shape)
--- utils/settings.py
+++ utils/settings.py
@@ -9,7 +9,10 @@ def create_directory(directory: str) -> None:
 
 emb_dir = os.path.join(root, emb_dir)
 ptm_emb_dir = os.path.join(emb_dir, ptm)
+dataset_dir = os.path.join(root, dataset_dir)
+
 create_directory(ptm_emb_dir)
 
-train_df_path = os.path.join(dataset_dir, f"{ptm}/train.csv")
-test_df_path = os.path.join(dataset_dir, f"{ptm}/test.csv")
+
+train_df_path = os.path.join(dataset_dir, f"{ptm}_train.csv")
+test_df_path = os.path.join(dataset_dir, f"{ptm}_test.csv")
